c033f4efe36e0970cc76c5b0041fa88a44550d90
Santiago Crisantes <crisantes@chapman.edu>
2020-11-16 15:40:30 -0800

    Updates the username.sh

--- username.sh
+++ username.sh
@@ -2,12 +2,12 @@
 # username.sh
 # Santiago Crisantes 
 echo "Enter a username: "
-echo
+echo "Only lowercase letters, digits, and the underscore letter"
 
 read -r  NAME 
 while echo "$NAME" | grep -E  -v "^[a-z][0-9a-z_]{2,11}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid NAME - five digits only!"
+	echo "You must enter a valid NAME"
 	echo "Enter a NAME: "
 	read -r  NAME 
 done
